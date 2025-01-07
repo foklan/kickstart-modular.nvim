9871c440a67ed77570887717d7f27b22c2ed46e5
Foklan <foklan@protonmail.com>
2025-01-07 08:08:42 +0100

    Remove all colorscheme plugins from custom init file

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,14 +2,4 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-  {
-    'uloco/bluloco.nvim',
-    lazy = false,
-    priority = 100000,
-    dependencies = { 'rktjmp/lush.nvim' },
-    config = function()
-      -- your optional config goes here, see below.
-    end,
-  },
-}
+return {}
