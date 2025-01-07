279b2c8a8b1f25addec35dd627c11b58fa01ef9f
Foklan <foklan@protonmail.com>
2025-01-07 08:10:09 +0100

    Add new colorscheme plugin file

---
+++ lua/custom/plugins/tokyodark.lua
@@ -0,0 +1,25 @@
+return {
+  'tiagovla/tokyodark.nvim',
+  priority = 1000,
+  init = function()
+    vim.cmd.colorscheme 'tokyodark'
+  end,
+  opts = {
+    transparent_background = false, -- set background to transparent
+    gamma = 0.9, -- adjust the brightness of the theme
+    styles = {
+      comments = { italic = true }, -- style for comments
+      keywords = { italic = true }, -- style for keywords
+      identifiers = { italic = true }, -- style for identifiers
+      functions = { bold = true }, -- style for functions
+      variables = {}, -- style for variables
+    },
+    custom_highlights = {} or function(highlights, palette)
+      return {}
+    end, -- extend highlights
+    custom_palette = {} or function(palette)
+      return {}
+    end, -- extend palette
+    terminal_colors = true, -- enable terminal colors
+  },
+}
